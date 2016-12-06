20e1e1fe666c322e4ebad41c460913282828b0ec
catalinwk <catalinwk@gmail.com>
2016-12-06 21:33:03 +0200

    Testing Simple Directory Listing
    Finds a imple way to list direcotry

---
+++ SimpleTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package audioLibraryManager;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ * This class test the console for changinc currentPath and listing files
+ * @author Catalin Mazilu
+ */
+public class SimpleTest {
+    
+    /*
+     * this is used to store current path
+     */
+    public static String currentPath = ".";
+   
+    /**
+     * List the directory specified
+     * @param direcotry name
+     */
+   public static void listDirectory(String param){
+       File path = new File(param);
+        String[] list;
+        
+        list = path.list();
+        System.out.println("Current path is " + path.getAbsolutePath());
+        
+        
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for(String dirItem : list)
+        System.out.println(dirItem);
+   }
+   
+   /**
+    * changes current directory
+    * @param param 
+    */
+   public static void changeDirectory(String param){
+       
+   }
+   
+   /**
+    * Creates an command line interface for working with files
+    * @param args 
+    */
+    
+    public static void main(String[] args){
+         String command = "";
+         System.out.println("Audio Library Manager\ntype 'help' for command list");
+         while (!command.contains("exit")){
+                       
+                Scanner s = new Scanner(System.in);
+                command = s.nextLine();
+               
+                if (command.contains("ls")) {
+                    
+                    System.out.println("Listing...");
+                    listDirectory("./");
+                }
+                
+               if (command.contains("cd"))
+                    System.out.println("Changing directory...");
+               
+               if (command.contains("help")) {
+                   System.out.println("help - getting commnmand list");
+                   System.out.println("ls [directory] lists direcotry contents");
+                   System.out.println("cd [directory] change current direcotry");
+                   
+                   
+               }
+                
+           
+        }
+        
+        
+    }
+    
+}
